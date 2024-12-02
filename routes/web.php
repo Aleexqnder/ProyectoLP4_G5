0a8254ebd06ff6d991fecf96b5b2c006c1a8c2d0
Aleexqnder <jasson.suazo@unah.hn>
2024-12-02 00:47:16 -0600

    Corrección

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ use App\Http\Controllers\UsuariosController;
 use App\Http\Controllers\CotizacionesController;
 use App\Http\Controllers\RegisterController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Definir la ruta para mostrar el formulario de registro
 Route::get('/registro', [RegisterController::class, 'index'])->name('register.index');
